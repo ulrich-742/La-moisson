e8bf6eca590dfebbb2e2362dd16144688dfb97c4
Thomas <thomas.soret@orange.fr>
2026-05-05 21:32:58 +0200

    Ajout

---
+++ src/components/RegistreRecolte.tsx
@@ -0,0 +1,14 @@
+const RegistreRecolte = () => {
+	return (
+		<div>
+			<div>
+				<h2>REGISTRE DE RECOLTE</h2>
+				<h4>Consultez l'historique de toutes vos récoltes</h4>
+				<button type="button">+ Enregistrer une récolte</button>
+			</div>
+			<button type="button">Voir tout le registre →</button>
+		</div>
+	);
+};
+
+export default RegistreRecolte;
