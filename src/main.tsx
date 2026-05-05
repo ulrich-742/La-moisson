01e416ee325e4ff9b1981f3a766647d1c576ba0c
Thomas <thomas.soret@orange.fr>
2026-05-05 22:02:08 +0200

    feat : design system créé dans index.css et import police Lora dans main.tsx et import bibliothèques Lucid pour les icones

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,9 @@
+import "@fontsource/cinzel/400.css";
+import "@fontsource/cinzel/600.css";
+import "@fontsource/cinzel/700.css";
+import "@fontsource/lora/400.css";
+import "@fontsource/lora/400-italic.css";
+import "@fontsource/lora/600.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
