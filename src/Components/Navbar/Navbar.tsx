237f956d6e6728a1d7eb8aa3b18ebc4693d08693
ulrich-742 <ulrichamoussoupro@gmail.com>
2026-05-06 09:24:39 +0200

    navbar end

---
+++ src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,59 @@
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+
+     <div className="navbar-left">
+          <div className="logo">
+            <span className="logo-icon">🌾</span>
+
+            <div className="logo-text">
+              <span className="title">LA MOISSON</span>
+
+              <div className="logo-bottom">
+                <span className="line"></span>
+                <span className="year">1600</span>
+                <span className="line"></span>
+              </div>
+            </div>
+          </div>
+      </div>
+
+      
+      <div className="navbar-center">
+
+        <NavLink to="/" className={({ isActive }) => isActive ? "nav-item active" : "nav-item"}>
+          🏠 Accueil
+        </NavLink>
+
+        <NavLink to="/moisson" className={({ isActive }) => isActive ? "nav-item active" : "nav-item"}>
+          🌾 La Moisson
+        </NavLink>
+
+        <NavLink to="/parcelles" className={({ isActive }) => isActive ? "nav-item active" : "nav-item"}>
+          🧱 Parcelles
+        </NavLink>
+
+        <NavLink to="/meteo" className={({ isActive }) => isActive ? "nav-item active" : "nav-item"}>
+          ☁️ Météo
+        </NavLink>
+
+        <NavLink to="/cours-grains" className={({ isActive }) => isActive ? "nav-item active" : "nav-item"}>
+          💰 Cours des grains
+        </NavLink>
+
+        <NavLink to="/registre" className={({ isActive }) => isActive ? "nav-item active" : "nav-item"}>
+          📜 Registre de récolte
+        </NavLink>
+
+        <NavLink to="/conseils" className={({ isActive }) => isActive ? "nav-item active" : "nav-item"}>
+          🧠 Conseils
+        </NavLink>
+
+      </div>
+
+    </nav>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import App from "./App";
+
+import Home from "./Components/Pages/Home";
+// import Moisson from "./assets/Components/Pages/Moisson";
+// import Parcelles from "./assets/Components/Pages/Parcelles";
+// import Meteo from "./assets/Components/Pages/Meteo";
+// import CoursGrains from "./assets/Components/Pages/CoursGrains";
+// import Registre from "./assets/Components/Pages/Registre";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
+    //   { path: "/moisson", element: <Moisson /> },
+    //   { path: "/parcelles", element: <Parcelles /> },
+    //   { path: "/meteo", element: <Meteo /> },
+    //   { path: "/coursgrains", element: <CoursGrains /> },
+    //   { path: "/registre", element: <Registre /> },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
 );
