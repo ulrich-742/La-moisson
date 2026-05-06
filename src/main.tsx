268f1fda6d75cb192964713cd4f365945fac58cd
Thomas <thomas.soret@orange.fr>
2026-05-06 12:33:54 +0200

    update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -12,30 +12,30 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
-import Home from "./Components/Pages/Home";
+import Home from "./components/Pages/Home";
 // import Moisson from "./Components/Pages/Moisson";
 // import Parcelles from "./Components/Pages/Parcelles";
 // import Meteo from "./Components/Pages/Meteo";
 // import CoursGrains from "./Components/Pages/CoursGrains";
-// import Registre from "./Components/Pages/Registre";
+import RegistreRecolte from "./components/RegistreRecolte/RegistreRecolte";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      { path: "/", element: <Home /> },
-      // { path: "/moisson", element: <Moisson /> },
-      // { path: "/parcelles", element: <Parcelles /> },
-      // { path: "/meteo", element: <Meteo /> },
-      // { path: "/cours-grains", element: <CoursGrains /> },
-      // { path: "/registre", element: <Registre /> },
-    ],
-  },
+	{
+		path: "/",
+		element: <App />,
+		children: [
+			{ path: "/", element: <Home /> },
+			// { path: "/moisson", element: <Moisson /> },
+			// { path: "/parcelles", element: <Parcelles /> },
+			// { path: "/meteo", element: <Meteo /> },
+			// { path: "/cours-grains", element: <CoursGrains /> },
+			{ path: "/registre", element: <RegistreRecolte /> },
+		],
+	},
 ]);
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>,
 );
