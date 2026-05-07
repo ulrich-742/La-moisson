da7e4c27d901d5afc589891561f0e31aa231345a
giobestava <gio.bestava17@gmail.com>
2026-05-07 11:34:36 +0100

    commented errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { Outlet } from "react-router-dom";
-import Navbar from "./Components/Navbar/Navbar";
-import Footer from "./Components/Footer/Footer";
+// import Navbar from "./Components/Navbar/Navbar";
+// import Footer from "./Components/Footer/Footer";
 
 
 /* const parcellesData = [
